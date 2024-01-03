3e803e62b5edce6d8816e33e5c859d2f648c7ad3
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-03 22:33:52 +0530

    Can be last exp v2

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -23,7 +23,7 @@ public class SavingsAccount extends BankAccount{
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        double SI = getBalance()*(rate/100)*years;
+        double SI = getBalance() * (rate/100) * years;
         return getBalance() + SI;
     }
 
