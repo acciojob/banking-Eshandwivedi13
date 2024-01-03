a8bd5adfe433d2b0995803989a67b36012dc7d90
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-03 16:48:45 +0530

    fifth

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -16,18 +16,62 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if(9*digits<sum || sum<0){
+        if(9*digits < sum){
             throw new AcNumberException("Account Number can not be generated");
         }
-        StringBuilder accNo = new StringBuilder();
-        // Start with the most significant digit
-        for (int i = 1; i <= digits; i++){
-            // Generate a random digit between 0 and 9 (both inclusive)
-            int digit = (int) (Math.random() * 10);//gives random decimal value and converting in integer
-            // Append the digit to the account number
-            accNo.append(digit);
+        String AcNo="";
+        for(int i=0;i<digits;i++)
+        {
+            if(sum>=9)
+            {
+                sum-=9;
+                AcNo += "9";
+            }
+            else if(sum>=8)
+            {
+                sum-=8;
+                AcNo+="8";
+            }
+            else if(sum>=7)
+            {
+                sum-=7;
+                AcNo+="7";
+            }
+            else if(sum>=6)
+            {
+                sum-=6;
+                AcNo+="6";
+            }
+            else if (sum>=5)
+            {
+                sum-=5;
+                AcNo+="5";
+            }
+            else if(sum>=4)
+            {
+                sum-=4;
+                AcNo+="4";
+            }
+            else if(sum>=3)
+            {
+                sum-=-3;
+                AcNo+="3";
+            }
+            else if(sum>=2)
+            {
+                sum-=2;
+                AcNo+="2";
+            }
+            else if(sum>=1)
+            {
+                sum-=1;
+                AcNo+="1";
+            }
+            else {
+                AcNo+="0";
+            }
         }
-        return accNo.toString();
+        return AcNo;
     }
 
     public double getMinBalance() {
